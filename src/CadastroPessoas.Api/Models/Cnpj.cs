d0e662530ed6cb9c4dabc036dc27d3e6b151a21f
Lucas Reis <contato@playevokids.com>
2026-09-13 19:21:12 -0300

    fix(api): evita ciclo de serializacao pessoa cnpj

--- src/CadastroPessoas.Api/Models/Cnpj.cs
+++ src/CadastroPessoas.Api/Models/Cnpj.cs
@@ -1,3 +1,7 @@
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
 namespace CadastroPessoas.Api.Models
 {
     public class Cnpj
@@ -8,6 +12,10 @@ namespace CadastroPessoas.Api.Models
 
         public int PessoaId { get; set; }
 
+        // Evita referência circular durante a serialização.
+        [JsonIgnore]
+        [IgnoreDataMember]
+        [XmlIgnore]
         public Pessoa Pessoa { get; set; }
     }
 }
