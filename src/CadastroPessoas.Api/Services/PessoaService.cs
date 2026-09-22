033225f9e68d3b85ea5491ce825b4e111699801e
Lucas Reis <contato@playevokids.com>
2026-09-21 22:15:57 -0300

    feat(api): persiste pessoas com Entity Framework

--- src/CadastroPessoas.Api/Services/PessoaService.cs
+++ src/CadastroPessoas.Api/Services/PessoaService.cs
@@ -1,30 +1,42 @@
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using CadastroPessoas.Api.Data;
 using CadastroPessoas.Api.Dtos;
 using CadastroPessoas.Api.Models;
 
 namespace CadastroPessoas.Api.Services
 {
-    public class PessoaService
+    public class PessoaService : IDisposable
     {
-        private readonly List<Pessoa> _pessoas = new List<Pessoa>();
-        private readonly object _sincronizacao = new object();
-        private int _proximoPessoaId = 1;
-        private int _proximoCnpjId = 1;
+        private readonly CadastroPessoasContext _context;
+
+        public PessoaService()
+            : this(new CadastroPessoasContext())
+        {
+        }
+
+        public PessoaService(CadastroPessoasContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            _context = context;
+        }
 
         public IReadOnlyCollection<Pessoa> Listar()
         {
-            lock (_sincronizacao)
-            {
-                return _pessoas.ToArray();
-            }
+            return _context.Pessoas
+                .Include(pessoa => pessoa.Cnpjs)
+                .ToList();
         }
 
         public Pessoa BuscarPorId(int id)
         {
-            lock (_sincronizacao)
-            {
-                return BuscarPorIdSemBloqueio(id);
-            }
+            return BuscarComCnpjs(id);
         }
 
         public ResultadoCriacaoPessoa Adicionar(CriarPessoaDto dto)
@@ -40,25 +52,22 @@ namespace CadastroPessoas.Api.Services
                 return CriarErro(mensagemErro);
             }
 
-            lock (_sincronizacao)
+            var pessoa = new Pessoa
             {
-                var pessoa = new Pessoa
-                {
-                    Id = _proximoPessoaId++,
-                    Nome = dto.Nome,
-                    Tipo = dto.Tipo,
-                    Cpf = dto.Cpf
-                };
+                Nome = dto.Nome,
+                Tipo = dto.Tipo,
+                Cpf = dto.Cpf
+            };
 
-                AdicionarCnpjs(pessoa, dto.Cnpjs);
-                _pessoas.Add(pessoa);
+            AdicionarCnpjs(pessoa, dto.Cnpjs);
+            _context.Pessoas.Add(pessoa);
+            _context.SaveChanges();
 
-                return new ResultadoCriacaoPessoa
-                {
-                    Sucesso = true,
-                    Pessoa = pessoa
-                };
-            }
+            return new ResultadoCriacaoPessoa
+            {
+                Sucesso = true,
+                Pessoa = pessoa
+            };
         }
 
         public ResultadoAtualizacaoPessoa Atualizar(int id, AtualizarPessoaDto dto)
@@ -74,47 +83,57 @@ namespace CadastroPessoas.Api.Services
                 return CriarErroAtualizacao(mensagemErro);
             }
 
-            lock (_sincronizacao)
+            var pessoa = BuscarComCnpjs(id);
+            if (pessoa == null)
             {
-                var pessoa = BuscarPorIdSemBloqueio(id);
-                if (pessoa == null)
-                {
-                    return new ResultadoAtualizacaoPessoa
-                    {
-                        Sucesso = false,
-                        PessoaNaoEncontrada = true,
-                        Mensagem = "Pessoa não encontrada.",
-                        Pessoa = null
-                    };
-                }
-
-                pessoa.Nome = dto.Nome;
-                pessoa.Tipo = dto.Tipo;
-                pessoa.Cpf = dto.Cpf;
-                pessoa.Cnpjs = new List<Cnpj>();
-                AdicionarCnpjs(pessoa, dto.Cnpjs);
-
                 return new ResultadoAtualizacaoPessoa
                 {
-                    Sucesso = true,
-                    PessoaNaoEncontrada = false,
-                    Pessoa = pessoa
+                    Sucesso = false,
+                    PessoaNaoEncontrada = true,
+                    Mensagem = "Pessoa não encontrada.",
+                    Pessoa = null
                 };
             }
+
+            pessoa.Nome = dto.Nome;
+            pessoa.Tipo = dto.Tipo;
+            pessoa.Cpf = dto.Cpf;
+            _context.Cnpjs.RemoveRange(pessoa.Cnpjs.ToList());
+            pessoa.Cnpjs.Clear();
+            AdicionarCnpjs(pessoa, dto.Cnpjs);
+            _context.SaveChanges();
+
+            return new ResultadoAtualizacaoPessoa
+            {
+                Sucesso = true,
+                PessoaNaoEncontrada = false,
+                Pessoa = pessoa
+            };
         }
 
         public bool Excluir(int id)
         {
-            lock (_sincronizacao)
+            var pessoa = BuscarPorId(id);
+            if (pessoa == null)
             {
-                var pessoa = BuscarPorIdSemBloqueio(id);
-                if (pessoa == null)
-                {
-                    return false;
-                }
-
-                return _pessoas.Remove(pessoa);
+                return false;
             }
+
+            _context.Pessoas.Remove(pessoa);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        private Pessoa BuscarComCnpjs(int id)
+        {
+            return _context.Pessoas
+                .Include(pessoa => pessoa.Cnpjs)
+                .SingleOrDefault(pessoa => pessoa.Id == id);
         }
 
         private static string ValidarDadosPessoa(
@@ -152,7 +171,7 @@ namespace CadastroPessoas.Api.Services
             return null;
         }
 
-        private void AdicionarCnpjs(Pessoa pessoa, ICollection<string> numerosCnpj)
+        private static void AdicionarCnpjs(Pessoa pessoa, ICollection<string> numerosCnpj)
         {
             if (numerosCnpj == null)
             {
@@ -161,21 +180,11 @@ namespace CadastroPessoas.Api.Services
 
             foreach (var numeroCnpj in numerosCnpj)
             {
-                var cnpj = new Cnpj
+                pessoa.Cnpjs.Add(new Cnpj
                 {
-                    Id = _proximoCnpjId++,
-                    Numero = numeroCnpj,
-                    PessoaId = pessoa.Id,
-                    Pessoa = pessoa
-                };
-
-                pessoa.Cnpjs.Add(cnpj);
+                    Numero = numeroCnpj
+                });
             }
-        }
-
-        private Pessoa BuscarPorIdSemBloqueio(int id)
-        {
-            return _pessoas.Find(pessoa => pessoa.Id == id);
         }
 
         private static ResultadoCriacaoPessoa CriarErro(string mensagem)
